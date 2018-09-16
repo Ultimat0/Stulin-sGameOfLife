e5f0c01d7743dbbde69f19f681f576c9b6c5f8fb
Mark N <marknazzaro2@gmail.com>
2018-09-16 17:41:19 -0400

    Initial Commit

---
+++ Stulin's Game of Life/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Stulin's Game of Life
+//
+//  Created by Masha Chausovsky on 9/16/18.
+//  Copyright © 2018 King Ultimat0's Fabulous Creations. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
